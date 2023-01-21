891ee8b31e5c08e7b4686576c759f973cf0f08af
anastasia <storojew.i123@gmail.com>
2023-01-21 08:05:30 +0300

    task 2

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,34 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        task1 ();
+        task2();
+    }
+    public static void task1 () {
+        System.out.println("Задача 1");
+    }
+    int [] number = new int [] {1,2,3};
+    double [] number2 = {1.57d, 7.654d, 9.986d};
+    int [] weekDays = {1,2,3,4,5,6,7};
+
+    public static void task2 () {
+        System.out.println("Задача 2");
+
+        int [] number = new int [] {1,2,3};
+        for (int i = 0; i < 3 ; i++) {
+            if (number [i] <= 2) {
+                System.out.print(number [i] + ",");
+            } else {
+                System.out.println(number [i]);
+            }
+        }
+        double [] number2 = {1.57d, 7.654d, 9.986d};
+        for (int i = 0; i < 2; i++) {
+            if (number2 [i] <= 2) {
+                System.out.print(number2 [i] + ",");
+            } else {
+                System.out.print(i);
+            }
+
+        }
     }
 }
